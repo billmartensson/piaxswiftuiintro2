ee3d5c7fc7ee346a1a73f59bf59387a42a3bc266
Bill Mårtensson <bill@magictechnology.se>
2021-11-04 09:07:41 +0100

    Initial Commit

---
+++ piaxswiftuiintro2/piaxswiftuiintro2App.swift
@@ -0,0 +1,17 @@
+//
+//  piaxswiftuiintro2App.swift
+//  piaxswiftuiintro2
+//
+//  Created by Bill Martensson on 2021-11-04.
+//
+
+import SwiftUI
+
+@main
+struct piaxswiftuiintro2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
